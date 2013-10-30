17a1fedba883c23edd6da755fb6f11fd4d841171
Raghu Betina <rbetina@gmail.com>
2013-10-30 16:52:32 -0500

    Attack of the chaos monkey :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Practice::Application.routes.draw do
   get("/", { :controller => "calculations", :action => "home" })
-  get("/square/:zebra", { :controller => "calculations", :action => "sq" })
   get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
   get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
