c4ba5d097447f7bfb31db763ce29355fa3751e61
Raghu Betina <rbetina@gmail.com>
2013-10-30 16:01:04 -0500

    Added square function

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,4 +1,9 @@
 class CalculationsController < ApplicationController
+  def sq
+    @ze_number = params[:zebra]
+    @ze_answer = @ze_number.to_f * @ze_number.to_f
+  end
+
   def sqrt
     @the_number = params[:number].to_f
     @answer = Math.sqrt(@the_number)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Practice::Application.routes.draw do
+  get("/square/:zebra", { :controller => "calculations", :action => "sq" })
   get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
 end
